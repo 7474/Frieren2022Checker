ce94b7d00cb3d40482be610564cf313102318293
koudenpa <koudenpa@hotmail.com>
2022-09-25 20:12:07 +0900

    取り合えずグラフ出た

--- Frieren2022Viewer/Program.cs
+++ Frieren2022Viewer/Program.cs
@@ -1,3 +1,5 @@
+using Blazorise;
+using Blazorise.Bootstrap;
 using Frieren2022Viewer;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -6,6 +8,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrapProviders();
 
 await builder.Build().RunAsync();
